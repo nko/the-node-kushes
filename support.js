c33baed85f85e7710881eac5f8b02e78038498b8
Jonas Galvez <jonas@codeazur.com.br>
2010-08-31 17:09:39 -0300

    Updating support.js, added links to each library.

--- support.js
+++ support.js
@@ -1,12 +1,30 @@
 require.paths.unshift(
   
-    './lib/connect/lib'
-  , './lib/express/lib'
-  , './lib/ejs/lib'
-  , './lib/socket.io/lib'
-  , './lib/commonjs-utils/lib'
-
+  // http://github.com/senchalabs/connect
+  './lib/connect/lib',
+  
+  // http://github.com/visionmedia/express
+  './lib/express/lib',
+  
+  // http://github.com/visionmedia/ejs
+  './lib/ejs/lib',
+  
+  // http://github.com/LearnBoost/Socket.IO-node
+  './lib/socket.io/lib',
+  
+  // http://github.com/kriszyp/commonjs-utils
+  './lib/commonjs-utils/lib',
+  
+  // http://github.com/brainfucker/hashlib
+  './lib/hashlib/build/default', // need to run 'make' first
+  
+  // http://github.com/fictorial/redis-node-client
+  './lib/redis-node-client/lib'
+  
 );
+sys = require("sys");
 express = require('express');
 JSON = require('json');
 io = require('socket.io');
+hashlib = require("hashlib");
+redis = require("redis-client");
